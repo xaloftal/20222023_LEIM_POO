62ea3bcdb6c2b143b79ab75448f3b54376fa338e
Ana Carvalho <anabeatriz0242@gmail.com>
2022-12-28 15:10:34 +0000

    add

---
+++ RNCCI.Data/Doentes.cs
@@ -0,0 +1,23 @@
+﻿using RNCCI.Enums;
+using RNCCI.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RNCCI.Dados
+{
+    public class Doentes
+    {
+
+        List<Doente> doentes = new List<Doente>();
+
+        public Doentes()
+        {
+            doentes.Add(new Doente { Nome = "Manel Figueiras", NumeroContribuinte = 4325, });
+
+
+        }
+    }
+}
